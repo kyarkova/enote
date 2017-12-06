f9c50add9b54228342f91e496e64d9d572613f55
Kseniia Iarkova <kseniia_iarkova@epam.com>
2017-12-06 23:34:44 +0300

    remove connection tag-user

--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -2,14 +2,18 @@
 DROP SCHEMA enote;
 CREATE SCHEMA enote;
 use  enote;
+
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(45) NOT NULL unique,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
 DROP TABLE IF EXISTS `notebook`;
+
 CREATE TABLE `notebook` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -18,7 +22,9 @@ CREATE TABLE `notebook` (
   UNIQUE(`name`,`user_id`),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 );
+
 DROP TABLE  IF EXISTS `note`;
+
 CREATE TABLE `note` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
@@ -26,16 +32,17 @@ CREATE TABLE `note` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`notebook_id`) REFERENCES `notebook` (`id`) ON DELETE CASCADE
 );
+
 DROP TABLE IF EXISTS `tag`;
+
 CREATE TABLE `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE(`name`,`user_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  `name` varchar(45) NOT NULL unique,
+  PRIMARY KEY (`id`)
 );
+
 DROP TABLE IF EXISTS `note_tag`;
+
 CREATE TABLE `note_tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `note_id` int(11) NOT NULL,
